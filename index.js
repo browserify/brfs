7c114050fc2af94a0df7abac5fa677eb608a707a
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-01-25 12:50:42 -0500

    Only parse JS files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var falafel = require('falafel');
 var unparse = require('escodegen').generate;
 
 module.exports = function (file) {
-    if (/\.json$/.test(file)) return through();
+    if (!/\.js$/.test(file)) return through();
     var data = '';
     var fsNames = {};
     var vars = [ '__filename', '__dirname' ];
