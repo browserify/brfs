49dcd97ff8bfb29d1a62791915eb01522b8404dd
mattdesl <dave.des@gmail.com>
2016-01-18 10:36:19 -0500

    new failing test

--- test/files/with_comments.js
+++ test/files/with_comments.js
@@ -1,19 +1,20 @@
 var
-  /**
-   * Dependencies.
-   */
-  fs = require('fs'),
+    /**
+     * Dependencies.
+     */
+    fs = require('fs'),
 
-  /**
-   * Local variables.
-   */
-  style = fs.readFileSync(__dirname + '/robot.html', 'utf8');
-console.log(style);
+    /**
+     * Local variables.
+     */
+    style = fs.readFileSync(__dirname + '/robot.html', 'utf8');
+
+
 module.exports = function () {
-  // var
-  //     tag = document.createElement('style'),
-  //     content = document.createTextNode(style);
+    var
+        tag = document.createElement('style'),
+        content = document.createTextNode(style);
 
-  // tag.appendChild(content);
-  // document.body.appendChild(tag);
+    tag.appendChild(content);
+    document.body.appendChild(tag);
 };
